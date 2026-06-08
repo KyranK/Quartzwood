d602a30ed7f520979ab34d16b22165306b8e6a18
Patch <kyran.k@outlook.com>
2026-06-08 14:14:21 +1000

    Generated storage.py in models

---
+++ quartzwood/models/storage.py
@@ -0,0 +1,9 @@
+from typing import Optional
+from sqlmodel import SQLModel, Field
+
+
+class Storage(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str
+    description: Optional[str] = None
+    collection_id: Optional[int] = Field(default=None, foreign_key="collection.id")
