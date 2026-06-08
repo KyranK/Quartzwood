5d672f59a6bd9cef1e2f29ff63d113216ad34a22
Patch <kyran.k@outlook.com>
2026-06-08 15:59:11 +1000

    Set name fields in sotrage & collection models to have the unquie paramater

--- quartzwood/models/collection.py
+++ quartzwood/models/collection.py
@@ -4,7 +4,7 @@ from sqlmodel import SQLModel, Field
 
 class Collection(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
-    name: str
+    name: str = Field(unique=True)
     description: Optional[str] = None
     location: Optional[str] = None
     # entity_id: Optional[int] = Field(default=None, foreign_key="entity.id")
--- quartzwood/models/storage.py
+++ quartzwood/models/storage.py
@@ -4,6 +4,6 @@ from sqlmodel import SQLModel, Field
 
 class Storage(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
-    name: str
+    name: str = Field(unique=True)
     description: Optional[str] = None
     collection_id: Optional[int] = Field(default=None, foreign_key="collection.id")
