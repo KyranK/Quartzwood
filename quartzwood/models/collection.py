95ec5d6903fe90981077078b92e0cb4b6db63430
Patch <kyran.k@outlook.com>
2026-06-08 14:09:17 +1000

    Generated Collection.py model

---
+++ quartzwood/models/collection.py
@@ -0,0 +1,10 @@
+from typing import Optional
+from sqlmodel import SQLModel, Field
+
+
+class Collection(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str
+    description: Optional[str] = None
+    location: Optional[str] = None
+    # entity_id: Optional[int] = Field(default=None, foreign_key="entity.id")
