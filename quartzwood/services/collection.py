6706d7c13d392275a37263a08d7f63d08f5d240f
Kyran Kerr <Kyran.k@outlook.com>
2026-07-02 19:42:35 +1000

    Bug - services/collection.py/->get_collections_by_entity_id was returning a pointer instead of a list

--- quartzwood/services/collection.py
+++ quartzwood/services/collection.py
@@ -33,7 +33,7 @@ def get_collection_by_name(session: Session, collection_name) -> Collection | No
 
 
 def get_collections_by_entity_id(session: Session, entity_id) -> list[Collection]:
-    collections = session.exec(select(Collection).where(Collection.entity_id == entity_id))
+    collections = session.exec(select(Collection).where(Collection.entity_id == entity_id)).all()
     return collections
 
     #endregion
