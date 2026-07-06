788431b4ad6c21858447058d2273f849a8f44d65
Kyran Kerr <Kyran.k@outlook.com>
2026-07-06 16:13:31 +1000

    Installed and init a ts React prj under ./frontend/

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import { Routes, Route } from 'react-router-dom'
+import HomePage from './pages/HomePage.tsx'
+import CollectionPage from './pages/CollectionPage.tsx'
+import StoragePage from './pages/StoragePage.tsx'
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/collection/:name" element={<CollectionPage />} />
+      <Route path="/storage/:name" element={<StoragePage />} />
+    </Routes>
+  )
+}
+
+export default App
